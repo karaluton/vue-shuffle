7bf3379b57a8122ffc483c21e6feb6c1603fd044
karaluton <karaluton@gmail.com>
2018-08-03 13:36:37 -0500

    Update vue instance to dynamically render cards

---
+++ main.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+        ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+        suits: ['♥','♦','♠','♣'],
+        cards: [],
+    },
+    created() {
+        this.displayInitialDeck();;
+    },
+    methods: {
+        displayInitialDeck() {
+            //
+        },
+    },
+});
